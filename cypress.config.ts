5e648fca5d83409b7b554bae490b9aa3808cf447
Valandi <valandi.allulis@gmail.com>
2022-09-22 16:34:56 -0400

    Quick TS Cypress 10 project integrated with eyes-cypress

---
+++ cypress.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'cypress';
+
+module.exports = defineConfig({
+  fixturesFolder: false,
+  video: false,
+  
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+    },
+  },
+})
+
+
+require('@applitools/eyes-cypress')(module);
